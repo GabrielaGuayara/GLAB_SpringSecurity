451391d57653f03f233d1ad12c54661cbf4b06fe
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-08-16 15:33:00 -0400

    GLAB to practice Spring security

---
+++ GLABSP14/src/main/java/com/glabSP14/Glabsp14Application.java
@@ -0,0 +1,13 @@
+package com.glabSP14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.glabSP14")
+public class Glabsp14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Glabsp14Application.class, args);
+	}
+
+}
---
+++ GLABSP14/src/test/java/com/glabSP14/Glabsp14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glabSP14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Glabsp14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
